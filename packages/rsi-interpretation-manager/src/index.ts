1041808523f911d7c901c6317a3695f7885b5b36
romain130492 <rouiller.romain11@gmail.com>
2021-06-24 15:57:30 +0800

    feat: added event language & interpretation language section

--- packages/rsi-interpretation-manager/src/index.ts
+++ packages/rsi-interpretation-manager/src/index.ts
@@ -1,18 +1,34 @@
+
+
+
+
+
 import RSIBase from '@akkadu/rsi-base'
 import {  languagesList } from './languages'
 /* import Logger from '@akkadu/logger' */
+
 export default class InterpretationManager extends RSIBase {
   apiKey: string; 
   $logger: any;
   container: string;
   domContainer: any;
+  eventLanguage: any;
+  buttonAddLanguage: any;
+  languageSelectorInterpretation: any;
+  isSelectorInterpretationLanguage:boolean;
+  isAddLanguageButtonOn : boolean;
+  interpretationLanguages : Array<any>;
   constructor( apiKey:string ) {
     super()
     this.apiKey = apiKey;
     this.container = 'akkadu-interpretation-manager'
+    this.isSelectorInterpretationLanguage = false;
+    this.buttonAddLanguage = null;
+    this.languageSelectorInterpretation = null;
+    this.isAddLanguageButtonOn = true;
+    this.interpretationLanguages = [];
     if(!document){ 
       throw Error('InterpretationManager: document is undefined.');
-      return
     }
     if(!this.apiKey){
       throw Error('InterpretationPlayer: apiKey is undefined')
@@ -26,11 +42,22 @@ export default class InterpretationManager extends RSIBase {
   async init(){
     this.addInterpretationManager()
   }
-  selectLanguage(language){
-    console.log('selected new language');
+
+  /**
+   * @description Call when the user selects a language from the dropdown "Event Languages:""
+   */
+  selectEventLanguage(language:any){
+    console.info('selected new language:', language);
+    this.eventLanguage = language;
   }
-  addInterpretationManager(){
-    console.log('mounted???');
+  selectInterpretationLanguage(language:any){
+    console.info('selected new interpretation language:', language);
+  }
+
+  /**
+   * @description Add the base which is the Html and Css of the widget-components.ts
+   */
+  setUpBaseInterpretationManager(){
     /** * @description widget: {html:string, css:string} */
     const widget = require('../lib/widget-component.js').default 
     this.domContainer.insertAdjacentHTML( 'beforeend', widget.html); 
@@ -38,9 +65,129 @@ export default class InterpretationManager extends RSIBase {
     var style = document.createElement('style');
     style.textContent =  widget.css;
     document.head.appendChild(style);
+  }
 
+  /**
+   * @description Take care of the interpretation Event Languages section of the interpretation manager
+   */
+  handleEventLanguagesSection(){
+    this.generateLanguageSelector('int-manager-event-language-selector','event-language')
+  }
+
+  /**
+   * @description Take care of the interpretation language section of the interpretation manager
+   */
+  handleInterpretationLanguagesSection(){
+    this.generateLanguageSelector('int-manager-interpretation-language-selector','interpretation-language')
+    const interpretationWrapper: any  = document.querySelector(`#int-manager-interpretation-wrapper`);
+
+    this.buttonAddLanguage = interpretationWrapper.getElementsByTagName('button')[0];
+    this.languageSelectorInterpretation = document.querySelector('#int-manager-interpretation-language-selector');
+    this.buttonAddLanguage.addEventListener("click", (e:any) => {
+      this.isAddLanguageButtonOn =! this.isAddLanguageButtonOn
+      this.switchBetweenButtonAndSelector(this.isAddLanguageButtonOn)
+    })
+  }
+  addLanguageToInterpretation(language:any){
+    const generateLanguageLabel  = (language:any) => {
+      const label = document.createElement('div');
+      label.className = 'int-manager-label-language';
+      label.id = `int-manager-${language.code}`;
+      const newText =  document.createElement('h3');
+      newText.textContent= language.name.en;
+      const cross =  document.createElement('span');
+      cross.className = 'cross'
+      cross.textContent= 'x'
+      cross.onclick = () => { 
+        this.deleteLanguageLabel(language)
+      }
+      const labelImage = document.createElement('img')
+      labelImage.src = this.getFlagUrl(language.code);
+      label.appendChild(labelImage);
+      label.appendChild(newText);
+      label.appendChild(cross);
+      return label;
+    }
+    if(!this.interpretationLanguages.includes(language)){
+      this.interpretationLanguages.push(language);
+      const label = generateLanguageLabel(language);
+      const rowLanguage = document.querySelector(`#int-manager-row-language`);
+      rowLanguage.appendChild(label) 
+    }
+  }
+
+  deleteLanguageLabel(language:any){
+    try {
+      this.interpretationLanguages = this.interpretationLanguages.filter(function(language){
+        return language.code !== language.code;
+      });
+      let node:any = document.getElementById(`int-manager-${language.code}`);
+      if (node.parentNode) {
+        node.parentNode.removeChild(node);
+      }
+    } catch (err) {
+      throw Error(`interpretationManager: deleteLanguageLabel() error ${err}`);
+    }
+  }
+  /**
+   * @description Switch between the "Add Language button" and the "language selector"
+   * For the Interpretation Language Section.
+   */
+  switchBetweenButtonAndSelector(isAddLanguageButtonOn:boolean){
+    if(isAddLanguageButtonOn){
+      // We hide the language Selector and show the button.
+      this.buttonAddLanguage.classList.remove('undisplay');
+      this.languageSelectorInterpretation.classList.add('undisplay');
+    } else{
+      // We hide the button and show the language selector.
+      this.buttonAddLanguage.classList.add('undisplay');
+      this.languageSelectorInterpretation.classList.remove('undisplay');
+    }
+  }
+
+  addToDomLanguageSelectorBase(divIdTarget:string, id:string){
+  const divElement = `<div class="select">
+                        <div class="selectWrapper">
+                          <div  class="selectCustom js-selectCustom js-selectCustom-${id}" >
+                            <div id="int-manager-custom-value" class="selectCustom-trigger int-manager-custom-value-${id}">  <img src=''/> <h3></h3> </div>
+                            <div id="int-manager-options" class="selectCustom-options int-manager-options-${id}"/>
+                          </div>
+                        </div>
+                    </div>`
+    const languageSelector : any = document.querySelector(`#${divIdTarget}`)
+    languageSelector.insertAdjacentHTML( 'afterbegin', divElement ); 
+  }
+
+  /**
+   * @description Create a language select tag
+   * It generates the languages options + flags based on the "languagesList"
+   */
+  generateLanguageSelector(divTarget:string, id:string){
+    const closeSelector = (elSelectCustom:any) =>{
+      elSelectCustom.classList.remove("isActive");
+    }
+    const updateHeader = (selectorHeader:any, newLanguage:any) => {
+      if(newLanguage){
+        const newLanguageName = newLanguage.name.en;
+        selectorHeader.getElementsByTagName("h3")[0].textContent = newLanguageName;
+        const image = selectorHeader.getElementsByTagName('img')[0]
+        image.src = this.getFlagUrl(newLanguage.code);
+      }else{
+        selectorHeader.getElementsByTagName("h3")[0].textContent = "Select a language"
+      }
+    }
+    // Language Selector
+    this.addToDomLanguageSelectorBase(divTarget, id)
+
+     // Init Dropdown header
+    const elSelectCustom : any = document.getElementsByClassName(`js-selectCustom-${id}`)[0];
+    const selectorHeader : any = document.getElementsByClassName(`int-manager-custom-value-${id}`)[0]
+    selectorHeader.getElementsByTagName("h3")[0].textContent = 'Select a language';
+
+
+    // Languages Options
     const languagesOptions = document.createDocumentFragment();
-    const createItemLanguage =(language:any, index:number) => {
+    const createItemLanguage = (language:any, index:number) => {
       let newOption : any
       let newImage : any
       let newText : any
@@ -48,7 +195,18 @@ export default class InterpretationManager extends RSIBase {
       newOption = document.createElement('div');
       newOption.className = 'selectCustom-option';
       newOption.id = index
-      newOption.onclick = () => { this.selectLanguage(language) }; 
+      if(id === 'event-language'){
+        newOption.onclick = () => { this.selectEventLanguage(language), closeSelector(elSelectCustom), updateHeader(selectorHeader,language) }; 
+      } else if(id === 'interpretation-language'){
+        newOption.onclick = () => { 
+          this.selectInterpretationLanguage(language),
+          closeSelector(elSelectCustom), 
+          updateHeader(selectorHeader, null)
+          this.isAddLanguageButtonOn = true;
+          this.addLanguageToInterpretation(language)
+          this.switchBetweenButtonAndSelector(this.isAddLanguageButtonOn)
+         }; 
+      }
       newText = document.createElement('h3');
       newText.textContent= language.name.en;
       newText.id = index;
@@ -64,26 +222,33 @@ export default class InterpretationManager extends RSIBase {
       languagesOptions.appendChild(createItemLanguage(language, i )); 
       i++
     });
-    document.getElementById('interpretation-manager-options')!.appendChild(languagesOptions);
+    document.getElementsByClassName(`int-manager-options-${id}`)[0]!.appendChild(languagesOptions);
+    
 
-    // Init Dropdown header
-    const elSelectCustom : any = document.getElementsByClassName("js-selectCustom")[0];
-    const elSelectCustomValue : any = document.getElementById('interpretation-manager-custom-value')
-    elSelectCustomValue.getElementsByTagName("h3")[0].textContent = 'Select a language';
-
-    // Toggle select on label click
-    elSelectCustomValue.addEventListener("click", (e:any) => {
+    // Listeners Languages Selector
+      // Toggle select on label click
+    selectorHeader.addEventListener("click", (e:any) => {
       elSelectCustom.classList.toggle("isActive");
     });
-
-    // close the custom select when clicking outside.
+    // Close the language selector when clicking outside.
     document.addEventListener("click", (e:any) => {
       const didClickedOutside = !elSelectCustom.contains(e.target);
       if (didClickedOutside) {
-        elSelectCustom.classList.remove("isActive");
+        closeSelector(elSelectCustom)
       }
     });
+
+    // Listeners Languages Options
+
   }
 
-
+  /**
+   * @description Add the interpretation Manager to the dom on the ID #akkadu-interpretation-manager
+   */
+  addInterpretationManager(){
+    this.setUpBaseInterpretationManager()
+    this.handleEventLanguagesSection()
+    this.handleInterpretationLanguagesSection()
+  }
 }
+
--- packages/rsi-interpretation-manager/src/widget-component.ts
+++ packages/rsi-interpretation-manager/src/widget-component.ts
@@ -3,102 +3,138 @@
 const component = {
   html:`
   <body>
-    <div id="wrapper-interpretation-manager">
-      <div class="interpretation-manager-row">
-        <div class="interpretation-manager-column">
-          <h2>Interpretation </h2>
-          <div class="interpretation-manager-event-languages">
+    <div id="wrapper-int-manager">
+     <h1 class="title">Interpretation </h1>
+      <div class="int-manager-row">
+        <div class="int-manager-column">
+          <div class="int-manager-event-languages">
             <h3>Event Languages </h3> 
-            <div class="select">
-              <div class="selectWrapper" >
-                <div class="selectCustom js-selectCustom">
-                  <div id="interpretation-manager-custom-value" class="selectCustom-trigger">  <img src=''/> <h3>test1 </h3> </div>
-                  <div id="interpretation-manager-options" class="selectCustom-options">
-                </div>
-              </div>
+            <div id="int-manager-event-language-selector"> </div>
+          </div>
+          <div id="int-manager-interpretation-wrapper">  
+            <h2>Interpretation Languages: </h2> 
+            <div id="int-manager-row-language">
+              <div id="int-manager-interpretation-language-selector" class="undisplay"> </div>
+              <button id="int-manager-add-language" class="button-add-language">Add Language + </button>
             </div>
           </div>
-        </div>
+      </div>
+      <div class="int-manager-column">
+      <h2> Manage Interpreters </h2>
       </div>
     </div>
   </body>
   `,
   css:`
 
-  #wrapper-interpretation-manager{
+  #wrapper-int-manager{
+    box-shadow: 3px 6px 7px 4px #e3e3e3;
     background-color: #F7FAFD;
     font-family: "Helvetica Neue", Arial, sans-serif;
+    width: 75%;
+    padding: 1rem 2rem 2rem 1rem;
+    margin: auto;
+    border-radius: 10px;
   }
-  #wrapper-interpretation-manager h1,
-  #wrapper-interpretation-manager h2, 
-  #wrapper-interpretation-manager h3, 
-  #wrapper-interpretation-manager p{
+  #wrapper-int-manager h1,
+  #wrapper-int-manager h2, 
+  #wrapper-int-manager h3, 
+  #wrapper-int-manager p{
     color: black;
     margin:0px;
     padding:0px;
   }
-  #wrapper-interpretation-manager h1{
-  
-  }
-  #wrapper-interpretation-manager h2{
-    font-size: 19px;
+  #wrapper-int-manager h1{
+    font-size:24px;
     padding:1rem 0rem;
   
   }
-  #wrapper-interpretation-manager h3{
+  #wrapper-int-manager h2{
+    font-size: 19px;
+    padding:0rem 0rem 1rem 0rem;
+  }
+
+  #wrapper-int-manager h3{
     font-size: 16px;
     color: #2C3D4F;
     padding:0rem 0rem;
   }
-  
-  #wrapper-interpretation-manager p{
+  #wrapper-int-manager button{
+    padding: 0.8rem 1rem;
+    color: white;
+  }
+  #wrapper-int-manager .button-add-language{
+    background: #2D72FE;
+    margin:0rem 1rem 1rem 0rem;
+  }
+  #wrapper-int-manager p{
   
   }
-  #wrapper-interpretation-manager img{
+  #wrapper-int-manager .title{
+    padding: 1rem 1rem 1.5rem 2rem;
+  }
+  .int-manager-column{
+    flex: 1 0 50%;
+    white-space: nowrap;
+    padding: 0rem 2rem;
+  }
+  .int-manager-column:nth-child(1){
+    border-right:1px solid #D8E0E8;
+  }
+
+  #wrapper-int-manager img{
       margin:0px;
       padding:0px;
       max-width: 20px;
       margin-right:10px;
   }
-  #wrapper-interpretation-manager button{
+  #wrapper-int-manager button{
       background:#58C283;
       border-radius: 20px;
       border: none;
       cursor: pointer;
   }
-  #wrapper-interpretation-manager button:hover{
+  #wrapper-int-manager button:hover{
     background: #56b77d;
   }
-  #wrapper-interpretation-manager button h3{
+  #wrapper-int-manager button h3{
       padding: 0px 20px;
       color:white;
   }
-  .interpretation-manager-event-languages{
+  .int-manager-event-languages{
+    display: table;
+    padding-bottom:1.5rem;
+  }
+  .int-manager-event-languages h3{
+    display: table-cell;
+    vertical-align: middle;
+    padding-right: 2rem !important;
+  }
+
+  .int-manager-row{
     display:flex;
   }
-  
-  
-  #interpretation-manager-options{
+  #int-manager-options{
     max-height: 175px;
     overflow-y: scroll;
   }
 
   
-  #wrapper-interpretation-manager .select {
+  #wrapper-int-manager .select {
       position: relative;
     }
-    #wrapper-interpretation-manager .selectLabel {
+    #wrapper-int-manager .selectLabel {
       display: block;
       margin-bottom: 10px;
     }
-    #wrapper-interpretation-manager .selectWrapper {
+    #wrapper-int-manager .selectWrapper {
       position: relative;
     }
-    #wrapper-interpretation-manager  .selectCustom {
+    #wrapper-int-manager  .selectCustom {
       position: relative;
       height: 100%;
     }
-    #wrapper-interpretation-manager .selectCustom-trigger {
+    #wrapper-int-manager .selectCustom-trigger {
       display: flex;
       position: relative;
       min-width: 150px;
@@ -110,10 +146,10 @@ const component = {
       width: 100%;
       height: 100%;
     }
-    #wrapper-interpretation-manager  h3, #wrapper-interpretation-manager .selectCustom-trigger{
+    #wrapper-int-manager  h3, #wrapper-int-manager .selectCustom-trigger{
       font-weight: 400;
     }
-    #wrapper-interpretation-manager .selectCustom-trigger::after {
+    #wrapper-int-manager .selectCustom-trigger::after {
       content: "▾";
       color:#908f8f;
       position: absolute;
@@ -122,10 +158,10 @@ const component = {
       right: 10px;
     }
     
-    #wrapper-interpretation-manager .selectCustom-trigger:hover {
+    #wrapper-int-manager .selectCustom-trigger:hover {
       box-shadow: 0 0 4px #e9e1f8;
     }
-    #wrapper-interpretation-manager .selectCustom-options {
+    #wrapper-int-manager .selectCustom-options {
       position: absolute;
       top: 45px;
       left: 0;
@@ -141,22 +177,22 @@ const component = {
       box-shadow: 3px 6px 7px 4px #e3e3e3;
     }
     
-    #wrapper-interpretation-manager .selectCustom.isActive .selectCustom-options {
+    #wrapper-int-manager .selectCustom.isActive .selectCustom-options {
       display: block;
       min-width: 260px;
     }
     
-    #wrapper-interpretation-manager .selectCustom-option {
+    #wrapper-int-manager .selectCustom-option {
       position: relative;
       padding: 10px 15px;
       display: flex;
     }
     
-    #wrapper-interpretation-manager  .selectCustom-option:hover {
+    #wrapper-int-manager  .selectCustom-option:hover {
       background-color: #F2F2F2;
     }
     
-    #wrapper-interpretation-manager  .selectCustom-option:not(:last-of-type)::after {
+    #wrapper-int-manager  .selectCustom-option:not(:last-of-type)::after {
       content: "";
       position: absolute;
       bottom: 0;
@@ -166,10 +202,36 @@ const component = {
     }
     
   
-    #wrapper-interpretation-manager .selectWrapper{ 
+    #wrapper-int-manager .selectWrapper{ 
       display: flex;
     }
-  
+    #int-manager-interpretation-wrapper .undisplay{
+      display:none;
+    }
+    #int-manager-row-language{
+      display: flex;
+      flex-wrap: wrap;
+    }
+    #int-manager-row-language .int-manager-label-language{
+      position: relative;
+      padding: 10px 15px;
+      display: flex;
+      box-shadow: 2px 4px 9px 1px #e3e3e3;
+      background: white;
+      border-radius: 5px;
+      margin:0rem 1rem 1rem 0rem;
+      
+    }
+    .js-selectCustom-interpretation-language{
+      margin-right:2rem;
+      min-width: 100px !important;
+    }
+    #int-manager-row-language .cross{
+      padding-left:0.5rem;
+      cursor:pointer;
+      color:#949494;
+    }
+    
   `
 }
 
