4f09afec4dc3e09a12a079668392316916e7e4f3
romain130492 <rouiller.romain11@gmail.com>
2021-06-30 17:54:50 +0800

    auto update

--- packages/rsi-vanilla/update-version.js
+++ packages/rsi-vanilla/update-version.js
@@ -31,10 +31,10 @@ if(tmpVersionFile && tmpVersionFile.tmpVersion){
 
 // We do not update our asset folder on AWS if the current packageJson version is equal to
 // the version in the tmpVersion file
-/*  if(tmpVersion === currentVersion){
+ if(tmpVersion === currentVersion){
   console.info("No need to update rsi-vanilla on AWS akkadu-assets bucket. The version didn't change");
   return
-}   */
+}   
 /**
  * @description We write a new tmpVersion file
  */
@@ -72,7 +72,7 @@ const updateAWS = (file, fileName) =>{
     var s3 = new AWS.S3();
     s3.putObject({
       Bucket: myBucket,
-      Key: `akkadu-rsi-widget/rsi-vanilla/${currentVersion}/index.min.js`,
+      Key: `akkadu-rsi-widget/rsi-vanilla/${currentVersion}/${fileName}.min.js`,
       Body: base64data,
       ACL: 'public-read'
     },function (resp) {
